3698f575631b823a6eeb0d5487e4c084c2cf5c8c
Fred Zhang <fredzqm@gmail.com>
2017-05-03 00:58:59 -0400

    add udp test codes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		String hostNameToJoin = args[0];				
-		DistributedHashTable dht = DistributedHashTable.getIntance();
-		dht.joinCluster(hostNameToJoin);
+	public static void main(String[] args) throws SocketException, UnknownHostException {
+		String hostNameToJoin = args[0];
+		// DistributedHashTable dht = DistributedHashTable.getIntance();
+		// dht.joinCluster(hostNameToJoin);
+		RequestParser s = new RequestParser();
+		Server.sendObject(new HelloRequest(), InetAddress.getByName(hostNameToJoin), RequestParser.PORT);
+		while (true)
+			;
 	}
 
 }
