68aaef0f92b010a113718f133f41401e3b6b14b4
Fred Zhang <fredzqm@gmail.com>
2017-05-03 01:13:35 -0400

    add HelloRequest

---
+++ src/main/java/HelloRequest.java
@@ -0,0 +1,16 @@
+import java.net.InetAddress;
+
+public class HelloRequest implements Request {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+	@Override
+	public void handleRequest(InetAddress addr) {
+		System.out.println("Hello world");
+	}
+
+}
