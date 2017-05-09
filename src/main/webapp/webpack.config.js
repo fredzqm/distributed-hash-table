3e29b5fafb1025ec63ccc9c4cb0976173b0aaa80
Fred Zhang <fredzqm@gmail.com>
2017-05-09 14:13:50 -0400

    set up react

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -1,15 +1,39 @@
+const path = require("path");
+
 module.exports = {
-    entry: './name-manager.jsx',
-    output: {
-        filename: './site/bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js/,
-                loader: 'babel'
-            }
-        ]
-    },
-    devtool: 'source-map'
-}
+  entry: {
+    "../resources/templates/main": "./component/main.tsx"
+  },
+  output: {
+    path: __dirname,
+    // path: path.resolve(__dirname, "client/src/browser_action")
+    filename: "[name].js"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+  },
+
+  resolveLoader: {
+    modules: [
+      "./node_modules"
+    ]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {test: /\.tsx?$/, loader: "awesome-typescript-loader", query: {
+        // Use this to point to your tsconfig.json.
+        configFileName: path.resolve(__dirname, "component/tsconfig.json")
+      }},
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      // {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
+    ]
+  }
+};
